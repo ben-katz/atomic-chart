05a14dbbba31f0b925d4967892fd3bbab1963ab7
Ben Katz <benkatz670@gmail.com>
2024-12-03 18:29:06 -0800

    og image changes

--- src/app/elements/[slug]/opengraph-image.tsx
+++ src/app/elements/[slug]/opengraph-image.tsx
@@ -14,12 +14,12 @@ export default async function Image({ params }: { params: { slug: string } }) {
 
     return new ImageResponse(
         (
-            <div tw="text-4xl bg-white flex items-center justify-center">
-                <div tw="relative w-full h-full aspect-square p-8 flex flex-col justify-center items-center border border-black dark:border-white">
+            <div tw="text-4xl w-full h-full bg-white flex items-center justify-center">
+                <div tw="relative w-full h-full p-8 flex flex-col justify-center items-center border border-black dark:border-white">
                     <span tw="absolute top-8 left-8 text-xl">{element?.number}</span>
                     <div tw="flex flex-col items-center gap-4 pb-8">
-                        <span tw="text-7xl font-bold">{element?.symbol}</span>
-                        <h1 tw="text-xl">{element?.name}</h1>
+                        <span tw="text-[64px] font-bold">{element?.symbol}</span>
+                        <h1 tw="text-[44px]">{element?.name}</h1>
                     </div>
                     <span tw="absolute bottom-8 text-sm">{element?.atomic_mass}</span>
                 </div>
