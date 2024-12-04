a250429f026a4fb7d3cbe8d095210bb9053caf50
Ben Katz <benkatz670@gmail.com>
2024-12-03 18:23:37 -0800

    fix og image

--- src/app/elements/[slug]/opengraph-image.tsx
+++ src/app/elements/[slug]/opengraph-image.tsx
@@ -14,14 +14,14 @@ export default async function Image({ params }: { params: { slug: string } }) {
 
     return new ImageResponse(
         (
-            <div className="text-4xl bg-white flex items-center justify-center">
-                <div className="relative w-full h-full max-w-[300px] aspect-square p-8 flex flex-col justify-center items-center border border-black dark:border-white">
-                    <span className="absolute top-8 left-8 text-xl">{element?.number}</span>
-                    <div className="flex flex-col items-center gap-4 pb-8">
-                        <span className="text-7xl font-bold">{element?.symbol}</span>
-                        <h1 className="text-xl">{element?.name}</h1>
+            <div tw="text-4xl bg-white flex items-center justify-center">
+                <div tw="relative w-full h-full max-w-[300px] aspect-square p-8 flex flex-col justify-center items-center border border-black dark:border-white">
+                    <span tw="absolute top-8 left-8 text-xl">{element?.number}</span>
+                    <div tw="flex flex-col items-center gap-4 pb-8">
+                        <span tw="text-7xl font-bold">{element?.symbol}</span>
+                        <h1 tw="text-xl">{element?.name}</h1>
                     </div>
-                    <span className="absolute bottom-8 text-sm">{element?.atomic_mass}</span>
+                    <span tw="absolute bottom-8 text-sm">{element?.atomic_mass}</span>
                 </div>
             </div>
         ),
