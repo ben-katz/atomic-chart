0b6e4cf5816d20cf461b06ecac9016dc5af81db4
Ben Katz <benkatz670@gmail.com>
2024-12-03 18:27:00 -0800

    fix og again

--- src/app/elements/[slug]/opengraph-image.tsx
+++ src/app/elements/[slug]/opengraph-image.tsx
@@ -15,7 +15,7 @@ export default async function Image({ params }: { params: { slug: string } }) {
     return new ImageResponse(
         (
             <div tw="text-4xl bg-white flex items-center justify-center">
-                <div tw="relative w-full h-full max-w-[300px] aspect-square p-8 flex flex-col justify-center items-center border border-black dark:border-white">
+                <div tw="relative w-full h-full aspect-square p-8 flex flex-col justify-center items-center border border-black dark:border-white">
                     <span tw="absolute top-8 left-8 text-xl">{element?.number}</span>
                     <div tw="flex flex-col items-center gap-4 pb-8">
                         <span tw="text-7xl font-bold">{element?.symbol}</span>
